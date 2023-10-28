df2c353d753fec9404d5cc9572040565c6012fa9
Steven <quezadasteven570@gmail.com>
2023-10-27 19:38:14 -0600

    modelo categorias

---
+++ app/Models/Product_category.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product_category extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'id','name'
+    ];
+
+    public function product_category()
+    {
+        return $this->belongsTo(Product_category::class);
+    }
+
+}
