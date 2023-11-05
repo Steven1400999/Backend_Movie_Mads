f9b9cd9dd6272cd685a30cb0e51e7472998fa01d
Steven <quezadasteven570@gmail.com>
2023-11-04 19:57:02 -0600

    react instalado

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -14,55 +14,39 @@ class RegisterController extends ResponseController
      * Display a listing of the resource.
      */
 
-    public function register(Request $request): JsonResponse
-    {
-        $validator = Validator::make($request->all(), [
-            'name' => 'required',
-            'email' => 'required|email',
-            'password' => 'required',
+     public function register (Request $request):JsonResponse{
+        $validator = Validator::make($request->all(),[
+            'name'      => 'required',
+            'email'     => 'required|email',
+            'password'  => 'required',
             'c_password'=> 'required|same:password',
-            'rol_id' => 'requiered',
-
         ]);
 
-        if ($validator->fails()) {
-            return $this->sendError(
-                'Validation Error.',
-                $validator->errors()
-            );
+        if ($validator->fails()){
+            return $this->sendError('Validation Error.',
+            $validator->errors());
         }
-
+        
         $input = $request->all();
         $input['password'] = bcrypt($input['password']);
         $user = User::create($input);
         $success['token'] = $user->createToken('MyApp')->accessToken;
         $success['name'] = $user->name;
 
-        return $this->sendRequest($success, 'User register successfully.');
-
-
-
-
-
+        return $this->sendRequest($success,'User register successfully.');
     }
 
-    public function login(Request $request): JsonResponse
-    {
-        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+    public function login(Request $request):JsonResponse{
+        if (Auth::attempt(['email' => $request->email, 'password'=>$request->password])) {
             $user = Auth::user();
             $success['token'] = $user->createToken('MyApp')->accessToken;
             $success['name'] = $user->name;
 
-            return $this->sendRequest(
-                $success,
-                'User login successfully.'
-            );
-        } else {
-            return $this->sendError(
-                'Unauthorized.',
-                ['error' => 'Unauthorized']
-            );
+            return $this->sendRequest($success,
+            'User login successfully.');
+        }else{
+            return $this->sendError('Unauthorized.',
+            ['error'=>'Unauthorized']);
         }
     }
-
 }
