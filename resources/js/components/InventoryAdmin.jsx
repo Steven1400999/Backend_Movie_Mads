37818eec1e0d08d4a60f5c7c8229157fe31c9485
Steven <quezadasteven570@gmail.com>
2023-11-11 17:52:09 -0600

    opciones

--- resources/js/components/InventoryAdmin.jsx
+++ resources/js/components/InventoryAdmin.jsx
@@ -1,103 +1,98 @@
 import React, { useEffect, useState } from 'react';
-import ReactDOM from 'react-dom/client';
-import { Button, Card, Stack, Spinner, Container, Row, Col, DropdownButton, Dropdown } from 'react-bootstrap';
-import Card_C from './Card_C';
+import { Button, Container, Row, Col, DropdownButton, Dropdown, Spinner } from 'react-bootstrap';
 import axios from 'axios';
 import InventoryAdminitem from './InventoryAdminItem';
-import InventoryAdminUpdateForm from './InventoryAdminUpdateForm';
-
 
 function InventoryAdmin() {
-    const [inventoryData, setinventoryData] = useState({})
+  const [inventoryData, setInventoryData] = useState([]);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("http://localhost/Proyecto_Inventario/public/api/inventory_index");
+        setInventoryData(response.data); 
+        console.log(response.data);
 
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
 
-    useEffect(() => {
-        const getUsers = async () => {
-            await axios.get("http://localhost/Proyecto_Inventario/public/api/inventory_index")
-                .then(function (response) {
-                    console.log(inventoryData);
-                    setinventoryData(response.data)
-                })
-                .catch(function (error) {
-                    // manejar error
-                    console.log(error);
-                })
-                .finally(function () {
-                    // siempre sera executado
-                });
+    fetchData();
+  }, []);
 
-        }
-        getUsers()
-    }, [])
-
-    if (!inventoryData.length) return
-    <Spinner animation="border" role="status">
-        <span className="visually-hidden">Loading...</span>
-    </Spinner>
-
-
+  if (!inventoryData.length) {
     return (
-        <>
-            <Row className="mb-3">
-                <Col>
-                    <DropdownButton id="dropdown-basic-button" title="Product">
-                        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-                    </DropdownButton>
-                </Col>
-
-                <Col>
-                    <DropdownButton id="dropdown-basic-button" title="Stock">
-                        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-                    </DropdownButton>
-                </Col>
-
-                <Col>
-                    <DropdownButton id="dropdown-basic-button" title="Admission Date">
-                        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-                    </DropdownButton>
-                </Col>
-
-                <Col>
-                    <DropdownButton id="dropdown-basic-button" title="Supplier">
-                        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-                    </DropdownButton>
-                </Col>
-
-                <Col>
-                    <Button variant="primary">Eliminar filtros</Button>
-                </Col>
-
-
-            </Row>
-             <hr />
-            <Container fluid>
-                <Row className="gap-6" >
-                    {inventoryData.map((inventory) => (
-                        <InventoryAdminitem key={inventory.id}
-                            id={inventory.id}
-                            product_id={inventory.product_id}
-                            stock={inventory.stock}
-                            admission_date={inventory.admission_date}
-                            supplier_id={inventory.supplier_id}
-
-                        />
-                    ))}
-
-                </Row>
-            </Container>
-
-
-        </>
+      <Spinner animation="border" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </Spinner>
     );
+  }
+  return (
+    <>
+     <Row className="mb-3">
+  <Col>
+    <DropdownButton id="dropdown-product" title="Product">
+      {inventoryData.map((inventory) => (
+        <Dropdown.Item key={inventory.id} href={`#/product/${inventory.product_id}`}>
+          {`${inventory.product_id} - Product Name`} {/* Asegúrate de tener la propiedad 'product_name' en tus datos */}
+        </Dropdown.Item>
+      ))}
+    </DropdownButton>
+  </Col>
+
+  <Col>
+    <DropdownButton id="dropdown-stock" title="Stock">
+      {inventoryData.map((inventory) => (
+        <Dropdown.Item key={inventory.id} href={`#/stock/${inventory.stock}`}>
+          {inventory.stock}
+        </Dropdown.Item>
+      ))}
+    </DropdownButton>
+  </Col>
+
+  <Col>
+    <DropdownButton id="dropdown-date" title="Admission Date">
+      {inventoryData.map((inventory) => (
+        <Dropdown.Item key={inventory.id} href={`#/date/${inventory.admission_date}`}>
+          {inventory.admission_date}
+        </Dropdown.Item>
+      ))}
+    </DropdownButton>
+  </Col>
+
+  <Col>
+    <DropdownButton id="dropdown-supplier" title="Supplier">
+      {inventoryData.map((inventory) => (
+        <Dropdown.Item key={inventory.id} href={`#/supplier/${inventory.supplier_id}`}>
+          {`${inventory.supplier_id} - Supplier Name`} {/* Asegúrate de tener la propiedad 'supplier_name' en tus datos */}
+        </Dropdown.Item>
+      ))}
+    </DropdownButton>
+  </Col>
+
+  <Col>
+    <Button variant="primary">Eliminar filtros</Button>
+  </Col>
+</Row>
+
+      <hr />
+      <Container fluid>
+        <Row className="gap-6">
+          {inventoryData.map((inventory) => (
+            <InventoryAdminitem
+              key={inventory.id}
+              id={inventory.id}
+              product_id={inventory.product_id}
+              stock={inventory.stock}
+              admission_date={inventory.admission_date}
+              supplier_id={inventory.supplier_id}
+            />
+          ))}
+        </Row>
+      </Container>
+    </>
+  );
 }
 
 export default InventoryAdmin;
